e95a355e7c06ee4fe23ba57a03512ef2dc032cba
Andrewwxo <74658753+Andrewwxo@users.noreply.github.com>
2021-10-15 19:12:54 +0300

    Update main.py
    Update filter_numbers

--- homework_01/main.py
+++ homework_01/main.py
@@ -1,4 +1,10 @@
-def power_numbers(Numbers):
+"""
+Домашнее задание №1
+Функции и структуры данных
+"""
+
+
+def power_numbers():
     square_list = []
     for i in Numbers:
         square_list.append(i ** 2)
@@ -11,21 +17,8 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def is_prime(Numbers):
-    list_prime = []
-
-    for i in Numbers:
-        k = 0
-        for j in range(2, i + 1):
-            if i % j == 0:
-                k += 1
-        if k <= 1:
-            list_prime.append(i)
-    print("Prime numbers: ", list_prime)
-
-
-def filter_numbers(Numbers, filter_types):
-
+def filter_numbers():
+    
     list = []
 
     if filter_types == ODD:
@@ -41,6 +34,15 @@ def filter_numbers(Numbers, filter_types):
 
         print("Even numbers: ", list)
 
+    elif filter_types == PRIME:
+        for i in Numbers:
+            k = 0
+            for j in range(2, i + 1):
+                if i % j == 0:
+                    k += 1
+            if k <= 1:
+                list.append(i)
+        print("Prime numbers: ", list)
 
     """
     функция, которая на вход принимает список из целых чисел,
@@ -52,9 +54,8 @@ def filter_numbers(Numbers, filter_types):
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
-
-
-if __name__ == '__main__':
+    
+ if __name__ == '__main__':
     numbers = (1, 2, 5, 7,)
     power_numbers(numbers)
 
@@ -64,4 +65,6 @@ if __name__ == '__main__':
     numbs2 = [2, 1, 3, 5, 4]
     filter_types = EVEN
     filter_numbers(numbs2, filter_types)
-    is_prime(numbers)
+    filter_types = PRIME
+    numbs3 = [3, 5, 6, 8, 13, 25]
+    filter_numbers(numbs3, filter_types)

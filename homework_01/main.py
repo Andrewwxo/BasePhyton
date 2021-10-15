aa4768fd4a4bd0b78854abc5f9dd3885793d5191
Andrewwxo <74658753+Andrewwxo@users.noreply.github.com>
2021-10-15 18:01:33 +0300

    Update main.py
    Done homework1

--- homework_01/main.py
+++ homework_01/main.py
@@ -1,16 +1,8 @@
-"""
-Домашнее задание №1
-Функции и структуры данных
-"""
-
-
-def power_numbers():
-    """
-    функция, которая принимает N целых чисел,
-    и возвращает список квадратов этих чисел
-    >>> power_numbers(1, 2, 5, 7)
-    <<< [1, 4, 25, 49]
-    """
+def power_numbers(Numbers):
+    square_list = []
+    for i in Numbers:
+        square_list.append(i ** 2)
+    print("Square of numbers: ", square_list)
 
 
 # filter types
@@ -19,7 +11,37 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
+def is_prime(Numbers):
+    list_prime = []
+
+    for i in Numbers:
+        k = 0
+        for j in range(2, i + 1):
+            if i % j == 0:
+                k += 1
+        if k <= 1:
+            list_prime.append(i)
+    print("Prime numbers: ", list_prime)
+
+
+def filter_numbers(Numbers, filter_types):
+
+    list = []
+
+    if filter_types == ODD:
+        for i in Numbers:
+            if i % 2 != 0:
+                list.append(i)
+        print("Odd numbers: ", list)
+
+    elif filter_types == EVEN:
+        for i in Numbers:
+            if i % 2 == 0:
+                list.append(i)
+
+        print("Even numbers: ", list)
+
+
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -30,3 +52,16 @@ def filter_numbers():
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+
+
+if __name__ == '__main__':
+    numbers = (1, 2, 5, 7,)
+    power_numbers(numbers)
+
+    numbs1 = [1, 2, 3]
+    filter_types = ODD
+    filter_numbers(numbs1, filter_types)
+    numbs2 = [2, 1, 3, 5, 4]
+    filter_types = EVEN
+    filter_numbers(numbs2, filter_types)
+    is_prime(numbers)
